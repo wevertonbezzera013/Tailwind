828cc4aa98471c157f42f2f118dc2b0f9e57dfbb
Weverton <wevertonbezzera013@gmail.com>
2022-12-05 21:03:06 -0300

    tailwind

---
+++ test/src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import SideBar from './Sidebar';
+
+function App() {
+  return (
+    <div className="flex">
+      <SideBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ test/src/Sidebar.js
@@ -0,0 +1,13 @@
+const SideBar = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary">
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+            <i>E</i>
+        </div>
+    );
+}
+
+export default SideBar;
